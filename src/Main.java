520c1bf08b0c781e17173664ed26aa725762f1cd
izme <izme@MacBook-Pro-izme.local>
2018-07-11 23:26:41 +0400

    initializing repository

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String s = getString();
+    }
+
+
+    public static String getString() {
+        return "s";
+    }
+}
